d4d31e96e14a37f7bce54cf2d7c1b1699b678a1d
Maxim Varfolomeyev <varfick@gmail.com>
2020-04-30 21:20:04 +0300

    changed goal behavior in EatTreatsGoal to use attention range and speed, the closest food entity in range is chosen, animal moves closer to the food before eating it new goal is now added directly in concrete animals mixins instead of the general AnimalEntity (reimplemented for chicken and cats)

---
+++ src/main/java/ru/maxvar/mcf/easyfeed/mixins/CatEntityMixin.java
@@ -0,0 +1,26 @@
+package ru.maxvar.mcf.easyfeed.mixins;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.maxvar.mcf.easyfeed.EatTreatsGoal;
+
+@Mixin(CatEntity.class)
+public abstract class CatEntityMixin extends TameableEntity {
+
+    @SuppressWarnings("unused")
+    protected CatEntityMixin(EntityType<? extends TameableEntity> type, World world) {
+        super(type, world);
+    }
+
+    @SuppressWarnings("unused")
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    public void addEatTreatsGoal(CallbackInfo ci) {
+        this.goalSelector.add(2, new EatTreatsGoal(this, 10D, 0.6D));
+    }
+}
