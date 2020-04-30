99e19d42200bc05e404ffd05a719703982c1ae13
Maxim Varfolomeyev <varfick@gmail.com>
2020-04-30 21:54:23 +0300

    added behavior to cows, ocelots, pigs, rabbits, sheep, turtles, wolves

---
+++ src/main/java/ru/maxvar/mcf/easyfeed/mixins/PigEntityMixin.java
@@ -0,0 +1,26 @@
+package ru.maxvar.mcf.easyfeed.mixins;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.maxvar.mcf.easyfeed.EatTreatsGoal;
+
+@Mixin(PigEntity.class)
+public abstract class PigEntityMixin extends AnimalEntity {
+
+    @SuppressWarnings("unused")
+    protected PigEntityMixin(EntityType<? extends AnimalEntity> type, World world) {
+        super(type, world);
+    }
+
+    @SuppressWarnings("unused")
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    public void addEatTreatsGoal(CallbackInfo ci) {
+        this.goalSelector.add(3, new EatTreatsGoal(this, 6D, 1.2D));
+    }
+}
---
+++ src/main/java/ru/maxvar/mcf/easyfeed/mixins/TurtleEntityMixin.java
@@ -0,0 +1,26 @@
+package ru.maxvar.mcf.easyfeed.mixins;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.TurtleEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.maxvar.mcf.easyfeed.EatTreatsGoal;
+
+@Mixin(TurtleEntity.class)
+public abstract class TurtleEntityMixin extends AnimalEntity {
+
+    @SuppressWarnings("unused")
+    protected TurtleEntityMixin(EntityType<? extends AnimalEntity> type, World world) {
+        super(type, world);
+    }
+
+    @SuppressWarnings("unused")
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    public void addEatTreatsGoal(CallbackInfo ci) {
+        this.goalSelector.add(1, new EatTreatsGoal(this, 8D, 1.1D));
+    }
+}
